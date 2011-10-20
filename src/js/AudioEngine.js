29e565c6690c7fc0b6d5f1dfdb828d9833566243
John Hornsby <john@interactivelabs.co.uk>
2011-10-20 12:20:22 +0100

    initial beta commit

--- src/js/AudioEngine.js
+++ src/js/AudioEngine.js
@@ -3,7 +3,9 @@ var AudioEngine = function(options){
 	this._contianer = options.element;
 	this._swf = options.swf;
 	this._namespace = options.namespace;
-	this._onReady = options.onReady;
+	this._preloadList = options.preloadList;
+	this._onPreloadComplete = options.onPreloadComplete;
+	
 	this._isFlashFallBack = false;
 	this._isFlashReady = false;
 	this._isJavaScriptReady = false;
@@ -13,6 +15,7 @@ var AudioEngine = function(options){
 	
 	this._preloadContainer;
 	this._instanceContainer;
+	this._preloadIncrement = 0;
 	this._preloadsObject = {};
 	this._instancesArray = [];
 	this._instanceIncrement = 0;
@@ -29,19 +32,17 @@ var AudioEngine = function(options){
 //PRIVATE
 //_______________________________________________________________________________________________
 /**
+* Called from constructor to get things started
 *@private
 */
 AudioEngine.prototype._init = function(){
-	//check if we need flash fallback
-	this._checkAudioSupport();
-	
-	//TEMP FLASH TESTING
-	//this._isFlashFallBack = true;
-	
+	this._checkAudioSupport();			//check if we need flash fallback
+	//this._isFlashFallBack = true;		//TEMP FLASH TESTING
 	this._build();
 };
 
 /**
+* Checks whether browser can support ogv then mp3 format, if not then fallback to Flash. Method sets _isFlashFallBack and _audioFormat
 *@private
 */
 AudioEngine.prototype._checkAudioSupport = function(){
@@ -51,10 +52,10 @@ AudioEngine.prototype._checkAudioSupport = function(){
 	var oggTest;
 	if(!!document.createElement('audio').canPlayType){
 		var audioElement=document.createElement("audio");
-		oggTest=audioElement.canPlayType('video/ogg; codecs="theora, vorbis"');
-		if (oggTest !== "probably"){
-			mp3Test=audioElement.canPlayType('video/mpeg: codecs="avc1.42E01E, mp4a.40.2"');
-			if (mp3Test !== "probably"){
+		oggTest=audioElement.canPlayType('audio/ogg');
+		if (oggTest !== "maybe" && oggTest !== "probably"){
+			mp3Test=audioElement.canPlayType('audio/mpeg');
+			if (mp3Test !== "maybe" && mp3Test !== "probably"){
 				this._isFlashFallBack = true;
 			}else{
 				this._audioFormat = "mp3";
@@ -65,7 +66,7 @@ AudioEngine.prototype._checkAudioSupport = function(){
 	}else{
 		this._isFlashFallBack = true;
 	}
-}  
+}; 
 
 /**
 *@private
@@ -79,12 +80,7 @@ AudioEngine.prototype._build = function(){
 		this._instanceContainer = create("div");
 		attr(this._instanceContainer,"id","audioEngineInstanceContainer");
 		append(this._contianer,this._instanceContainer);
-		var self = this;
-		setTimeout(function(){
-			if(self.onReady != undefined){
-				self.onReady.apply(this);
-			}
-		},100);
+		this._onAllIsReady();
 	}else{		
 		var objectString = "";
 		objectString += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="300" height="300" id="audioEngineFlashPlayer">'
@@ -93,105 +89,30 @@ AudioEngine.prototype._build = function(){
 		objectString += '<param name="flashvars" value="namespace='+this._namespace+'">';
 		objectString += '<embed src="'+this._swf+'" quality="high" FlashVars="namespace='+this._namespace+'" width="300" height="300" name="audioEngineFlashPlayer" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />';
 		objectString += '</object>';
-		
 		this._contianer.innerHTML = objectString;
-		
 		this._flashReadyInterval = setInterval(this._FInterface_checkFlashIsReady.context(this),100);
-		
 	}
 };
 
-AudioEngine.prototype._F2JS_messageToJavaScript = function(str){
-	console.log(str);
+AudioEngine.prototype._onAllIsReady = function(){
+	//now preload 
+	this._preload(this._preloadList);
 };
-AudioEngine.prototype._F2JS_isReady = function(){
-	
-	console.log("_F2JS_isReady");
-	this._isJavaScriptReady = true;
-	
-	this._FInterface_checkBothAreReady();
-	return true;
-};
-
-AudioEngine.prototype._FInterface_checkBothAreReady = function(){
-	if(this._isJavaScriptReady === true && this._isFlashReady === true){
-		var self = this;
-		setTimeout(function(){
-			if(self._onReady != undefined){
-				self._onReady.apply(self);
-			}
-		},100);
-	}
-}
-
-AudioEngine.prototype._FInterface_checkFlashIsReady = function(){
-	var result = false;
-	try{
-		if (navigator.appName.indexOf("Microsoft") != -1) {
-			 this._flashElement = window["audioEngineFlashPlayer"];
-		 } else {
-			 this._flashElement = document.getElementsByName("audioEngineFlashPlayer")[0];
-		 }
-		result = this._flashElement._JS2F_isFlashReady();
-	}catch(e){
-		console.log("_FInterface_checkFlashIsReady Can't connect to Flash");	
-	}
-	if(result === true){
-		clearInterval(this._flashReadyInterval);	
-		this._isFlashReady = true;
-		
-		this._FInterface_checkBothAreReady();
-	}
-	console.log("_F2JS_checkFlashIsReady:"+this._isFlashReady);	
-}
-
-AudioEngine.prototype._FInterface_preload = function(preloadObjects){
-	this._flashElement._JS2F_preload(preloadObjects);
-};
-
-AudioEngine.prototype._FInterface_create = function(label,autoDestroy,autoRewind,autoStop){
-	if(autoDestroy == undefined)autoDestroy = true;
-	if(autoRewind == undefined)autoStop = false;
-	if(autoStop == undefined)autoDestroy = true;
-	return this._flashElement._JS2F_create(label,autoDestroy,autoRewind,autoStop);
-};
-
-AudioEngine.prototype._FInterface_play = function(label,id){
-	if(id === undefined) id = -1;
-	this._flashElement._JS2F_play(label,id);
-};
-
-AudioEngine.prototype._FInterface_pause = function(label,id){
-	if(id === undefined) id = -1;
-	this._flashElement._JS2F_pause(label,id);
-};
-
-AudioEngine.prototype._FInterface_stop = function(label,id){
-	if(id === undefined) id = -1;
-	this._flashElement._JS2F_stop(label,id);
-};
-
-AudioEngine.prototype._FInterface_stopAll = function(){
-	this._flashElement._JS2F_stopAll();
-};
-
-AudioEngine.prototype._FInterface_setVolume = function(volume){
-	this._flashElement._JS2F_setVolume(volume);
-};
-
-
-
-
-
-
 
 /**
 *@private
 */
 AudioEngine.prototype._preloadComplete = function(e){
-	console.log('_preloadComplete ' + attr(e.currentTarget,"data-label"));
+	console.log('_preloadComplete ' + attr(e.currentTarget,"data-label") + ' timestamp:'+e.timeStamp);
 	var label = attr(e.currentTarget,"data-label");
 	this._preloadsObject[label].isPreloaded = true;
+	this._preloadIncrement++;
+	
+	removeEvent(e.currentTarget,'canplaythrough',this._preloadComplete.rEvtContext(this));	//Firefox sometimes calls this event twice
+	if(this._preloadIncrement === this._preloadList.length){
+		//console.log("_preloadComplete");
+		this._onPreloadComplete();
+	}
 };
 
 /**
@@ -203,6 +124,7 @@ AudioEngine.prototype._createAudioInstance = function(object,autoDestroy,autoRew
 	attr(element,"src",object[this._audioFormat]);
 	attr(element,"data-label",object.label);
 	attr(element,"data-instance",this._instanceIncrement);
+	attr(element,"controls","controls");
 	append(this._instanceContainer,element);
 	addEvent(element,'ended',this._audioEnded.context(this));
 	
@@ -281,24 +203,23 @@ AudioEngine.prototype._removeAudioInstance = function(audioInstanceItem){
 		this._instancesArray.splice(index,1);
 		remove(audioInstanceItem.element);
 	}
-}
-
-
-//PUBLIC
-//_______________________________________________________________________________________________
+};
 
 /**
 * Preload array of sound objects
 * @public
 */
-AudioEngine.prototype.preload = function(a){
+AudioEngine.prototype._preload = function(a){
 	if(this._isFlashFallBack === true){
 		this._FInterface_preload(a);
 	}else{
 		var l;
 		var label;
+		var preloadObject;
 		for(var i=0,l = a.length; i<l; i++){
+			preloadObject = a[i];
 			label = a[i].label;
+			
 			if(this._preloadsObject[label] == undefined){
 				
 				this._preloadsObject[label] = a[i];
@@ -306,14 +227,120 @@ AudioEngine.prototype.preload = function(a){
 				a[i].isPreloaded = false;
 				attr(a[i].preloadElement,"src",a[i][this._audioFormat]);
 				attr(a[i].preloadElement,"data-label",a[i].label);
+				attr(a[i].preloadElement,"controls","controls");
 				append(this._preloadContainer,a[i].preloadElement);
-				addEvent(a[i].preloadElement,'canplaythrough',this._preloadComplete.context(this));
-				a[i].preloadElement.play()
-				a[i].preloadElement.pause();
+				addEvent(a[i].preloadElement,'canplaythrough',this._preloadComplete.rEvtContext(this));
+				
+				
+				if (navigator.appName.indexOf("Microsoft") == -1) {
+					//Firefox 7.01 OSX seemed to abort loading of longer audio files, therefore not able to preload, 
+					//however in IE 9 PC the audio preloads played but did not respond to pause.
+					a[i].preloadElement.play();
+					a[i].preloadElement.pause();
+				}
+				
 			}
 		}
 	}
 };
+
+
+
+
+
+//Flash Fallback Interface
+//_________________________________________________________________________________________________
+AudioEngine.prototype._F2JS_messageToJavaScript = function(str){
+	console.log(str);
+};
+AudioEngine.prototype._F2JS_isReady = function(){
+	
+	console.log("_F2JS_isReady");
+	this._isJavaScriptReady = true;
+	
+	this._FInterface_checkBothAreReady();
+	return true;
+};
+
+AudioEngine.prototype._F2JS_preloadComplete = function(){
+	//console.log("_F2JS_preloadComplete");
+	this._onPreloadComplete();
+};
+
+AudioEngine.prototype._FInterface_checkBothAreReady = function(){
+	if(this._isJavaScriptReady === true && this._isFlashReady === true){
+		this._onAllIsReady();
+	}
+};
+
+AudioEngine.prototype._FInterface_checkFlashIsReady = function(){
+	var result = false;
+	try{
+		if (navigator.appName.indexOf("Microsoft") != -1) {
+			 this._flashElement = window["audioEngineFlashPlayer"];
+		 } else {
+			 this._flashElement = document.getElementsByName("audioEngineFlashPlayer")[0];
+		 }
+		result = this._flashElement._JS2F_isFlashReady();
+	}catch(e){
+		console.log("_FInterface_checkFlashIsReady Can't connect to Flash");	
+	}
+	if(result === true){
+		clearInterval(this._flashReadyInterval);	
+		this._isFlashReady = true;
+		
+		this._FInterface_checkBothAreReady();
+	}
+	console.log("_F2JS_checkFlashIsReady:"+this._isFlashReady);	
+};
+
+AudioEngine.prototype._FInterface_preload = function(preloadObjects){
+	this._flashElement._JS2F_preload(preloadObjects);
+};
+
+AudioEngine.prototype._FInterface_create = function(label,autoDestroy,autoRewind,autoStop){
+	if(autoDestroy == undefined)autoDestroy = true;
+	if(autoRewind == undefined)autoStop = false;
+	if(autoStop == undefined)autoDestroy = true;
+	return this._flashElement._JS2F_create(label,autoDestroy,autoRewind,autoStop);
+};
+
+AudioEngine.prototype._FInterface_play = function(label,id){
+	if(id === undefined) id = -1;
+	this._flashElement._JS2F_play(label,id);
+};
+
+AudioEngine.prototype._FInterface_pause = function(label,id){
+	if(id === undefined) id = -1;
+	this._flashElement._JS2F_pause(label,id);
+};
+
+AudioEngine.prototype._FInterface_stop = function(label,id){
+	if(id === undefined) id = -1;
+	this._flashElement._JS2F_stop(label,id);
+};
+
+AudioEngine.prototype._FInterface_stopAll = function(){
+	this._flashElement._JS2F_stopAll();
+};
+
+AudioEngine.prototype._FInterface_setVolume = function(volume){
+	this._flashElement._JS2F_setVolume(volume);
+};
+//End of Flash Fallback Interface
+//_________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+//PUBLIC
+//_______________________________________________________________________________________________
 
 /**
 * Create sound and return instance id
